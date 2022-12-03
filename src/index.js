6fa05df8d2a7721539c164958ec8de5d06c1c4e7
sunchenggao <sunchenggao.lbf@gmail.com>
2022-12-04 00:38:08 +0900

    有两个敌人在移动

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ function SimpleGame() {
       style={{ width: 800, height: 600, backgroundColor: "green" }}
       systems={[MoveEnemy]}
       entities={{enemy1: { x: 200, y: 200, renderer: <Enemy /> },
+      enemy2: { x: 250, y: 200, renderer: <Enemy /> },
                  player: { x: 400, y: 400, renderer: <Player /> }}}
     />
   );
--- src/systems.js
+++ src/systems.js
@@ -8,9 +8,26 @@ export function MoveEnemy(entities, { input }) {
       player.y = payload.pageY;
     }
 
-    const enemy1 = entities["enemy1"];
+    let enemy1 = entities["enemy1"];
     enemy1.x = enemy1.x + 1;
     enemy1.y = enemy1.y + 1;
+
+    if(enemy1.x > 800){
+        enemy1.x = 0;
+    }
+    if(enemy1.x < 0){
+        enemy1.x = 800;
+    }
+    if(enemy1.y > 600){
+        enemy1.y = 0;
+    }
+    if(enemy1.y < 0){
+        enemy1.y = 600;
+    }
+
+    enemy1 = entities["enemy2"];
+    enemy1.x = enemy1.x + 3;
+    enemy1.y = enemy1.y - 1;
 
     if(enemy1.x > 800){
         enemy1.x = 0;
